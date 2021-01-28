955ed6cab1f31abf96e064703c9baa3506b9f25f
Abah Joseph Israel <abahjosephisrael@gmail.com>
2021-01-28 19:39:17 +0100

    built the mvc project to consume youtube api

--- MyPlaylistMvc/Controllers/HomeController.cs
+++ MyPlaylistMvc/Controllers/HomeController.cs
@@ -4,8 +4,19 @@ using MyPlaylistMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using MyPlaylistMvc.ViewModels;
 
 namespace MyPlaylistMvc.Controllers
 {
@@ -22,16 +33,53 @@ namespace MyPlaylistMvc.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<IActionResult> Search( [FromQuery] string query)
         {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            List<VideoVM> videos = new List<VideoVM>();
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = query; 
+            searchListRequest.MaxResults = 10;
+            
+            try
+            {
+
+                // Call the search.list method to retrieve results matching the specified query term.
+                var searchListResponse = await searchListRequest.ExecuteAsync();
+
+                // Add each result to the appropriate list, and then display the lists of
+                // matching videos, channels, and playlists.
+                foreach (var searchResult in searchListResponse.Items)
+                {
+                    
+
+                    if (searchResult.Id.Kind == "youtube#video")
+                    {
+                        var video = new VideoVM()
+                        {
+                            Description = searchResult.Snippet.Description,
+                            title = searchResult.Snippet.Title,
+                            image = searchResult.Snippet.Thumbnails.Default__.Url
+                        };
+                        videos.Add(video);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View(videos);
         }
     }
 }
---
+++ MyPlaylistMvc/ViewModels/VideoVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPlaylistMvc.ViewModels
+{
+    public class VideoVM
+    {
+        public string title { get; set; }
+        public string image { get; set; }
+        public string Description { get; set; }
+    }
+}
